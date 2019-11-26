7c70207f6f4023c3447c772bbb28a630a045a953
yanguangying <Yanguangying@whalesdesign.com>
2019-11-26 11:14:08 +0800

    refactor:调整灵敏度测试

--- js/app/main.js
+++ js/app/main.js
@@ -57,10 +57,10 @@ var main = Bone.extend({}, Bone.Events, {
             _self.aim.lat = obj.lat;
             _self.aim.lon = -obj.lon;
 
-            if (_self.lock) {
-                _self.fix.lat = -_self.aim.lat;
-                _self.fix.lon = -_self.aim.lon;
-            }
+            // if (_self.lock) {
+            //     _self.fix.lat = -_self.aim.lat;
+            //     _self.fix.lon = -_self.aim.lon;
+            // }
         };
         orienter.init();
 
@@ -84,7 +84,7 @@ var main = Bone.extend({}, Bone.Events, {
         this.animateId = requestAnimationFrame(this.animate);
 
         var _lon = (this.aim.lon + this.fix.lon + this.drag.lon) % 360;
-        var _lat = (this.aim.lat + this.fix.lat + this.drag.lat) * 0.35;
+        var _lat = (this.aim.lat + this.fix.lat + this.drag.lat) * 0.25;
 
         if (_lon - this.root.rotationY > 180) this.root.rotationY += 360;
         if (_lon - this.root.rotationY < -180) this.root.rotationY -= 360;
