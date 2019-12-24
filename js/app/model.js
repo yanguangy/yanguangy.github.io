1b5bad0c9d680c7850e6fec23416ddb52a1b8d35
yanguangying <Yanguangying@whalesdesign.com>
2019-12-24 15:30:18 +0800

    feat:微信分享样式调整

--- js/app/model.js
+++ js/app/model.js
@@ -33,37 +33,33 @@ var model = Bone.extend({}, Bone.Events, {
         // 发送请求:
         request.open('GET', 'http://api.whalesdesign.com/v1/wechat/getJsSdk?url=http://h5.whalesdesign.com');
         request.send();
-
-    // 配置分享到微信
-    wx.ready(function () {
-    
-    var shareData = {
-        title: "精于设计品牌介绍",
-        desc: '创意·设计·实现，设计与科技驱动的企业级解决方案',
-        link: "http://h5.whalesdesign.com",
-        imgUrl: '../../bg/logo.png',
-        success: function () { 
-            // 用户确认分享后执行的回调函数
-            PluginMessage.success({
-                text: "分享成功",
-            })
-        }
-    };
-    wx.updateAppMessageShareData(shareData);
-    wx.updateTimelineShareData(shareData);
-    wx.onMenuShareAppMessage(shareData);
-    wx.onMenuShareTimeline(shareData);
-    wx.onMenuShareQQ(shareData);
-    wx.onMenuShareQZone(shareData);
-});
-
-
-
-
-       
     },
     random: function (n1, n2) {
         // 获取随机数n1到n2之间的随机数
         return n1 + Math.floor(Math.random() * (n2 - n1) * 100) / 100;
     },
 });
+
+
+        // 配置分享到微信
+        wx.ready(function () {
+        
+            var shareData = {
+                title: "精于设计品牌介绍",
+                desc: '创意·设计·实现，设计与科技驱动的企业级解决方案',
+                link: "http://h5.whalesdesign.com",
+                imgUrl: '../../bg/logo.png',
+                success: function () { 
+                    // 用户确认分享后执行的回调函数
+                    PluginMessage.success({
+                        text: "分享成功",
+                    })
+                }
+            };
+            wx.updateAppMessageShareData(shareData);
+            wx.updateTimelineShareData(shareData);
+            wx.onMenuShareAppMessage(shareData);
+            wx.onMenuShareTimeline(shareData);
+            wx.onMenuShareQQ(shareData);
+            wx.onMenuShareQZone(shareData);
+        });
